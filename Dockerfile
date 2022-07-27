45feee56f11a5f74a050837a4124ec500a888c20
Urip Subekti <urip.subekti13@gmail.com>
2022-07-27 16:38:21 +0700

    fix: add title

--- Dockerfile
+++ Dockerfile
@@ -2,14 +2,15 @@ FROM node:14.20.0-alpine as base
 
 WORKDIR /app
 COPY package.json yarn.lock ./
-RUN yarn install --frozen-lockfile
+RUN yarn install --frozen-lockfile && yarn build
 
 FROM node:14.20.0-alpine
 ENV PORT=2000
 WORKDIR /app
 
 COPY --from=base /app/node_modules ./node_modules
+COPY --from=base /app/.next ./.next
 COPY . .
 
 EXPOSE 2000
-CMD [ "yarn", "dev" ]
+CMD [ "yarn", "start" ]
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,16 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from "@chakra-ui/react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
-	<ChakraProvider>
-		<Component {...pageProps} />
-	</ChakraProvider>
-	)
+    <ChakraProvider>
+      <Head>
+        <title>Bookmark App</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
